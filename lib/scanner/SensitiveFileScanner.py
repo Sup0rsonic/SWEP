794f57ea98b33eea35c60180a3be899a7c2a0def
Sup0rsonic <echostudios@foxmail.com>
2019-02-05 19:51:05 +0800

    Improved scanner

--- lib/scanner/SensitiveFileScanner.py
+++ lib/scanner/SensitiveFileScanner.py
@@ -3,6 +3,7 @@ import threading
 import queue
 import json
 import os
+import time
 
 
 def info():
@@ -10,16 +11,17 @@ def info():
         'name': 'file',
         'path': 'SensitiveFileScanner',
         'fullname': 'SWEP SENSITIVE FILE SCANNER',
-        'description': 'A simple sensitive file scanner.',
+        'description': 'A extra simple sensitive file scanner.',
         'parameters': {
             'Url': 'Target URL.',
             'Threads': 'Threads. Default: 10',
-            'SensitiveFileDict': '(OPTIOAL) Sensitive file dictionary',
+            'SensitiveFileDict': '(OPTIONAL) Sensitive file dictionary',
+            'Mode': '(OPTIONAL) Wordlist mode(json/plain)',
             'Protocol': 'Protocol. Default: http',
             'Timeout': 'Request timeout. Default: 3'
         },
-        'author': 'BREACHER security',
-        'date': '2019-01-12'
+        'author': 'BREACHERS security',
+        'date': '2019-02-05'
     }
     return info
 
@@ -32,15 +34,16 @@ class Scanner():
         self._Counter = None
         self.queue = queue.Queue()
         self.Protocol = 'http'
+        self.Mode = 'plain'
         self.UrlList = []
+        self.TaskList = []
         self.Timeout = 3
-        self.Name = 'SensitiveFile'
+        self.Status = False
         self.Path = os.path.dirname(os.path.abspath(__file__))
-        self.ThreadLock = False
         return
 
 
-    def LoadFileList(self):
+    def LoadJson(self):
         try:
             if not self.SensitiveFileDict:
                 print '[*] Dictionary not specified, Using default.'
@@ -53,31 +56,52 @@ class Scanner():
                 return
             if not FileList:
                 return
-            NewList = []
             for item in FileList:
-                if item not in NewList:
-                    NewList.append(item)
-                    self.queue.put(item)
-            print '[+] File load success, Total %s item(s).' % (str(len(NewList)))
+                self.Append(item)
+            print '[+] File load success, Total %s item(s).' % (self.queue.qsize())
         except Exception, e:
             print '[!] Failed to load dictionary: %s' %(str(e))
             return
-        return NewList
+        return
 
 
+    def LoadFileList(self):
+        if not os.path.isfile(str(self.SensitiveFileDict)):
+            print '[!] Invalid filename, Using default wordlist.'
+            self.SensitiveFileDict = '%s/../../WordList/SensitiveFile' %(self.Path)
+        for item in open(str(self.SensitiveFileDict)):
+            self.Append(item)
+        for ext in ['zip', 'rar', '7z']:
+            self.Append(self.Url.replace('.', '') + ext)
+            self.Append(self.Url.split('.')[-2] + ext)
+        return
+
+
+    def Append(self, item):
+        if item not in self.queue.queue:
+            self.queue.put(item)
 
 
     def GetSensitiveFile(self):
-        FileList = self.LoadFileList()
+        if self.Mode != 'json':
+            self.LoadFileList()
+        else:
+            self.LoadJson()
+        self.Status = True
+        checker = threading.Thread(target=self.ThreadChecker)
+        checker.setDaemon(True)
+        checker.start()
         while True:
             if self.Threads > self._Counter:
                 thread = threading.Thread(target=self.GetPage, args=[self.queue.get()])
                 thread.start()
+                self.TaskList.append(thread)
                 if not self.queue.qsize():
-                    thread.join()
+                    for item in self.TaskList:
+                        item.join()
+                    self.Status = False
                     break
-        while not self.ThreadLock:
-            return self.UrlList
+        return self.UrlList
 
         
         
@@ -120,6 +144,16 @@ class Scanner():
             print '[!] Failed to get sensitive file: %s' %(str(e))
             self.UrlList = []
         return self.UrlList
+
+
+    def ThreadChecker(self):
+        time.sleep(1)
+        while self.Status:
+            for item in self.TaskList:
+                if not item.isAlive():
+                    self.TaskList.remove(item)
+        return
+
 
     def info(self):
         InformationList = info()
